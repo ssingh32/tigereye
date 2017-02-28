3cbd52fe14697edcb40288f440771f764fab681e
sukhdipsingh <s_singh26@u.pacific.edu>
2017-02-28 00:14:32 -0800

    adding Events table/model

--- tigereyeAPP/models.py
+++ tigereyeAPP/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Events(models.Model):
+    start_time = models.DateTimeField
+    end_time = models.DateTimeField
+    location = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    category = models.CharField(max_length=100)
+    event_image = models.CharField(max_length=1000)
+
