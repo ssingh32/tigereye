021f3473a7d381ffa003a5f8b07f9a7f8e55d3b6
sukhdipsingh <s_singh26@u.pacific.edu>
2017-03-04 13:17:04 -0800

    Updating events page view to show all events and create hyperlink

--- tigereyeAPP/views.py
+++ tigereyeAPP/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from . models import Events
 
 def index(request):
-    return HttpResponse("<h1>This is the events homepage</h1>")
+    all_events = Events.objects.all()
+    html = ''
+    for event in all_events:
+        url = '/events/' + str(Events.id) + '/'
+        html += '<a href= "' + url + '">' + event.name + '</a><br>'
+    return HttpResponse(html)
 
 
 def detail(request, events_id):
