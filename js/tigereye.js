c531db819a67a97d80e3c5bca67a456a3dfc9a13
cookerlyk <kylecookerly@gmail.com>
2017-03-26 11:52:54 -0700

    Adding the JS to print database contents to HTML tables

--- js/tigereye.js
+++ js/tigereye.js
@@ -8,7 +8,7 @@
 //   });
 // });
 
-// On page load, the entire database contents are inserted into the results
+// On page load, the entire database contents are inserted into the corresponding
 // table by getting the JSON data from get_event_data.php
 $(window).on("load", function() {
     $.getJSON('/php/get_event_data.php', function(events) {
@@ -24,8 +24,36 @@ $(window).on("load", function() {
             var cost='<td style=width:10%>'+ val.Cost +'</td>';
             var end_row='</tr>';
 
-            // add the row to the table
+            // add the row to the events table
             $('#events-table').append(start_row+name+description+location+date+start+end+cost+end_row); 
+        });
+    });
+    $.getJSON('/php/get_club_data.php', function(events) {
+        $.each(events, function(key, val) {
+            var start_row='<tr id="found-player">';
+            //var image='<td style=width:25%>'+ val.Image +'</td>';
+            var name='<td style=width:25%>'+ val.Name +'</td>';
+            var description='<td style=width:10%>'+ val.Description +'</td>';
+            var location='<td style=width:10%>'+ val.Location +'</td>';
+            var cost='<td style=width:10%>'+ val.Cost +'</td>';
+            var end_row='</tr>';
+
+            // add the row to the clubs table
+            $('#clubs-table').append(start_row+name+description+location+cost+end_row); 
+        });
+    });
+    $.getJSON('/php/get_social_data.php', function(events) {
+        $.each(events, function(key, val) {
+            var start_row='<tr id="found-player">';
+            //var image='<td style=width:25%>'+ val.Image +'</td>';
+            var name='<td style=width:25%>'+ val.Name +'</td>';
+            var description='<td style=width:10%>'+ val.Description +'</td>';
+            var location='<td style=width:10%>'+ val.Location +'</td>';
+            var cost='<td style=width:10%>'+ val.Cost +'</td>';
+            var end_row='</tr>';
+
+            // add the row to the social areas table
+            $('#socialareas-table').append(start_row+name+description+location+cost+end_row); 
         });
     });
 });
