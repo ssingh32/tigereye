4710c15f4bb03964c257828d86855999decdaae5
cookerlyk <kylecookerly@gmail.com>
2017-03-26 11:42:54 -0700

    Removed a few comments and made a couple of comments clearer

--- database/tigereye_queries.sql
+++ database/tigereye_queries.sql
@@ -1,11 +1,11 @@
--- Grab from events db --
+-- Grab from events table
 SELECT DISTINCT * 
 FROM Events
 
--- Grab from clubs db --
+-- Grab from clubs table
 SELECT DISTINCT * 
 FROM Clubs
 
--- Grab from social areas db --
+-- Grab from social areas table
 SELECT DISTINCT * 
 FROM SocialAreas
