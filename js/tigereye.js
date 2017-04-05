ff065a8409efe66a8cabeb56a275b6f941f971de
cookerlyk <kylecookerly@gmail.com>
2017-04-05 10:52:35 -0700

    See more and see less buttons now work for the clubs and the social areas

--- js/tigereye.js
+++ js/tigereye.js
@@ -3,9 +3,10 @@
 */
 
 $(document).ready(function () {
-    $("#see-more-events").click(function () {
+    // see more events button
+    $("#see-more-events").click(function() {
         $("#events-div").empty();
-        $.getJSON('/php/get_event_data.php', function (events) {
+        $.getJSON('/php/get_event_data.php', function(events) {
             $.each(events, function (key, val) {
                 var row = '<div id="events" class="row">'
                 var event_start = '<div id="event" class="col-md-4">'
@@ -21,12 +22,14 @@ $(document).ready(function () {
                 $('#events-div').append(event_start + image + event_info);
             });
         });
-        $('#events-div').append('<button class="btn btn-primary btn-sm" id="see-less-events" onclick="">See Less Events<span class="dropup"><span class="caret"></button>');
+        $("#events-div").append('<button class="btn btn-primary btn-sm" id="see-less-events" onclick="">See Less Events<span class="dropup"><span class="caret"></span></button>');
     });
-    $("#see-more-clubs").click(function () {
+
+    // see more clubs button
+    $(document.body).on("click", "#see-more-clubs", function() {
         $("#clubs-div").empty();
-        $.getJSON('/php/get_club_data.php', function (events) {
-            $.each(events, function (key, val) {
+        $.getJSON('/php/get_club_data.php', function(clubs) {
+            $.each(clubs, function (key, val) {
                 var row = '<div id="clubs" class="row">'
                 var club_start = '<div id="club" class="col-md-4">'
                 var image = '<div id="image-div" class="col-md-4"><img id="event-image" src="../assets/blank_image.png" class="img-responsive"></div>'
@@ -37,12 +40,14 @@ $(document).ready(function () {
                 $('#clubs-div').append(club_start + image + club_info);
             });
         });
-        $('#clubs-div').append('<button class="btn btn-primary btn-sm" id="see-less-clubs" onclick="">See Less Clubs<span class="dropup"><span class="caret"></button>');
+        $("#clubs-div").append('<button class="btn btn-primary btn-sm" id="see-less-clubs" onclick="">See Less Clubs<span class="dropup"><span class="caret"></span></button>');
     });
-    $("#see-more-social").click(function () {
+
+    // see more social button
+    $(document.body).on("click", "#see-more-social",function() {
         $("#social-areas-div").empty();
-        $.getJSON('/php/get_social_data.php', function (events) {
-            $.each(events, function (key, val) {
+        $.getJSON('/php/get_social_data.php', function(social) {
+            $.each(social, function (key, val) {
                 var row = '<div id="social-areas" class="row">'
                 var social_start = '<div id="social-area" class="col-md-4">'
                 var image = '<div id="image-div" class="col-md-4"><img id="event-image" src="../assets/blank_image.png" class="img-responsive"></div>'
@@ -53,15 +58,40 @@ $(document).ready(function () {
                 $('#social-areas-div').append(social_start + image + social_info);
             });
         });
-        $('#social-areas-div').append('<button class="btn btn-primary btn-sm" id="see-less-social" onclick="">See Less Social Areas<span class="dropup"><span class="caret"></button>');
+        $("#social-areas-div").append('<button class="btn btn-primary btn-sm" id="see-less-social" onclick="">See Less Social Areas<span class="dropup"><span class="caret"></span></button>');
+    });
+
+    // see less events button
+    $(document.body).on("click", "#see-less-events",function() {
+        $("#events-div").empty();
+        display_3_events();
+    });
+
+    // see less clubs button
+    $(document.body).on("click", "#see-less-clubs",function() {
+        $("#clubs-div").empty();
+        display_3_clubs();
+    });
+
+    // see less social areas button
+    $(document.body).on("click", "#see-less-social",function() {
+        $("#social-areas-div").empty();
+        display_3_social();
     });
 });
 
 
 // On page load, the first 3 database contents are inserted into the corresponding
 // rows by getting the JSON data from each table in the DB
-$(window).on("load", function () {
-    $.getJSON('/php/get_event_data.php', function (events) {
+$(window).on("load", function() {
+    display_3_events();
+    display_3_clubs();
+    display_3_social();
+});
+
+// build the events div array with the first 3 entries
+function display_3_events() {
+    $.getJSON('/php/get_event_data.php', function(events) {
         var counter = 0;
         $.each(events, function (key, val) {
             var row = '<div id="events" class="row">'
@@ -82,9 +112,14 @@ $(window).on("load", function () {
             return counter < 3;
         });
     });
-    $.getJSON('/php/get_club_data.php', function (events) {
+    $('#events-div').append('<button class="btn btn-primary btn-sm" id="see-more-events" onclick="">See More Events<span class="caret"></span></button>')
+}
+
+// build the clubs div array with the first 3 entries
+function display_3_clubs() {
+    $.getJSON('/php/get_club_data.php', function(clubs) {
         var counter = 0;
-        $.each(events, function (key, val) {
+        $.each(clubs, function (key, val) {
             var row = '<div id="clubs" class="row">'
             var club_start = '<div id="club" class="col-md-4">'
             var image = '<div id="image-div" class="col-md-4"><img id="event-image" src="../assets/blank_image.png" class="img-responsive"></div>'
@@ -99,9 +134,14 @@ $(window).on("load", function () {
             return counter < 3;
         });
     });
-    $.getJSON('/php/get_social_data.php', function (events) {
+    $('#clubs-div').append('<button class="btn btn-primary btn-sm" id="see-more-clubs" onclick="">See More Clubs<span class="caret"></span></button>');
+}
+
+// build the social areas div array with the first 3 entries
+function display_3_social() {
+    $.getJSON('/php/get_social_data.php', function(social) {
         var counter = 0;
-        $.each(events, function (key, val) {
+        $.each(social, function (key, val) {
             var row = '<div id="social-areas" class="row">'
             var social_start = '<div id="social-area" class="col-md-4">'
             var image = '<div id="image-div" class="col-md-4"><img id="event-image" src="../assets/blank_image.png" class="img-responsive"></div>'
@@ -116,4 +156,5 @@ $(window).on("load", function () {
             return counter < 3;
         });
     });
-});
+    $('#social-areas-div').append('<button class="btn btn-primary btn-sm" id="see-more-social" onclick="">See More Social Areas<span class="caret"></span></button>');
+}
