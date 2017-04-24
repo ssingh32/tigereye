a12069934ca27518904edd1fe6fb3aeb6425873d
cookerlyk <kylecookerly@gmail.com>
2017-04-24 12:30:20 -0700

    Fixing bug where no matching events message would not display on an empty search input

--- php/search.php
+++ php/search.php
@@ -3,23 +3,29 @@
 header('Content-type: text/json');
 include_once("tigereyeDB.php");
 
-if(isset($_POST['search_term'])
-    && !empty($_POST['search_term'])
-){
+if(isset($_POST['search_term'])){
 
-$condition = "%".$_POST['search_term']."%";
+    if (!empty($_POST['search_term'])) {
 
-$sql = "SELECT DISTINCT * FROM Events WHERE Name LIKE " . "'". $condition. "'";
+        // condition to look for any events that contain the term anywhere in the name
+        $condition = "%".$_POST['search_term']."%";
 
-// Execute the query on the database
-$query = $tigereyeDB->prepare($sql);
-$query->execute();
+        $sql = "SELECT DISTINCT * FROM Events WHERE Name LIKE " . "'". $condition. "'";
 
-// Fetches the data from the DB
-$result = $query->fetchAll();
+        // Execute the query on the database
+        $query = $tigereyeDB->prepare($sql);
+        $query->execute();
 
-// echo JSON encoded data that was fetched from the database
-echo json_encode($result);
+        // Fetches the data from the DB
+        $result = $query->fetchAll();
+
+        // echo JSON encoded data that was fetched from the database that matched the search term
+        echo json_encode($result);
+    }
+    else {
+        // no results because the search term field was empty
+        echo json_encode(0);
+    }
 
 }
 
